4fdfd77664430c3b0414c312b9e08d3bf955069c
Khrystyna <khrystynavysotska@gmail.com>
2021-10-24 13:16:29 +0300

    Api paths and base url

---
+++ src/constants/apiPath.js
@@ -0,0 +1,10 @@
+export const RIVER_PATH = "/river/";
+
+export const LOCALITY_PATH = "/locality/";
+
+export const MEASUREMENT_PATH = "/measurement/";
+
+export const FLOAT_SENSOR_PATH = "/float_sensor/";
+
+export const MEASUREMENT_POINT_PATH = "/measurement_point/";
+
---
+++ src/constants/url.js
@@ -0,0 +1,1 @@
+export const BASE_DB_URL = "https://cloud-lab-backend-e7k64ndk7q-ey.a.run.app";
