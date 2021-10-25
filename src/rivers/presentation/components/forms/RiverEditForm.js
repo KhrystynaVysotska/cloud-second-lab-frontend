f72aaab49ff839a08c146265eb779f4c86b2697d
Khrystyna <khrystynavysotska@gmail.com>
2021-10-26 01:10:01 +0300

    Edit river form

---
+++ src/rivers/presentation/components/forms/RiverEditForm.js
@@ -0,0 +1,121 @@
+import React from "react";
+import RiverEditFormStyled from "../../../../styles/rivers/components/forms/RiverEditForm.styled";
+import BackgroundShadow from "../../../../styles/common/components/modals/BackgroundShadow";
+import Input from "@mui/material/Input";
+import { useDispatch } from "react-redux";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import { updateExistingRiver } from "../../../business/middleware/riverMiddleware";
+
+export default function RiverEditForm({
+  river,
+  showEditForm,
+  setShowEditForm,
+}) {
+  const dispatch = useDispatch();
+  const [name, setName] = React.useState(river.name);
+  const [image, setImage] = React.useState(river.image_url);
+  const [length, setLength] = React.useState(river.length_in_km);
+  const [basinArea, setBasinArea] = React.useState(river.basin_area_in_sq_km);
+
+  const onCancel = () => {
+    setName(river.name);
+    setLength(river.length_in_km);
+    setBasinArea(river.basin_area_in_sq_km);
+    setShowEditForm(false);
+  };
+
+  const onSubmit = () => {
+    dispatch(
+      updateExistingRiver(river.id, {
+        name: name,
+        length_in_km: length,
+        basin_area_in_sq_km: basinArea,
+        image_url: image,
+      })
+    )
+      .then((data) => {
+        setName(data.name);
+        setLength(data.length_in_km);
+        setBasinArea(data.basin_area_in_sq_km);
+        setImage(data.image_url);
+        setShowEditForm(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleNameInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleLengthInputChange = (e) => {
+    setLength(e.target.value);
+  };
+
+  const handleBasinAreaInputChange = (e) => {
+    setBasinArea(e.target.value);
+  };
+
+  return (
+    <BackgroundShadow show={showEditForm} onClick={onCancel}>
+      <RiverEditFormStyled
+        show={showEditForm}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <p className="header">Редагувати річку {river.name}</p>
+        <div className="form">
+          <img src={image} alt="river" className="image" />
+          <div className="inputs">
+            <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "50ch" }}>
+              <InputLabel htmlFor="name">Назва *</InputLabel>
+              <Input
+                required
+                label="Назва"
+                id="name"
+                value={name}
+                onChange={handleNameInputChange}
+              />
+            </FormControl>
+            <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "50ch" }}>
+              <InputLabel htmlFor="length">Довжина *</InputLabel>
+              <Input
+                type="number"
+                required
+                label="Довжина"
+                id="length"
+                value={length}
+                onChange={handleLengthInputChange}
+                endAdornment={
+                  <InputAdornment position="end">км</InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControl variant="standard" sx={{ m: 1, mt: 3, width: "50ch" }}>
+              <InputLabel htmlFor="basin_area">Площа басейну *</InputLabel>
+              <Input
+                required
+                type="number"
+                label="Площа басейну"
+                id="basin_area"
+                value={basinArea}
+                onChange={handleBasinAreaInputChange}
+                endAdornment={
+                  <InputAdornment position="end">км²</InputAdornment>
+                }
+              />
+            </FormControl>
+          </div>
+        </div>
+        <div className="buttons">
+          <button className="cancel" onClick={onCancel}>
+            Скасувати
+          </button>
+          <button className="apply" onClick={onSubmit}>
+            Застосувати
+          </button>
+        </div>
+      </RiverEditFormStyled>
+    </BackgroundShadow>
+  );
+}
---
+++ src/styles/rivers/components/forms/RiverEditForm.styled.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+const RiverEditFormStyled = styled.div`
+  margin: auto;
+  width: 900px;
+  display: flex;
+  height: 550px;
+  position: relative;
+  border-radius: 20px;
+  transition: all 0.3s;
+  flex-direction: column;
+  background-color: #fffffff2;
+  -webkit-transition: all 0.3s;
+  -webkit-transform: ${({ show }) => (show ? "scale(1)" : "scale(0.7)")};
+  -ms-transform: ${({ show }) => (show ? "scale(1)" : "scale(0.7)")};
+  transform: ${({ show }) => (show ? "scale(1)" : "scale(0.7)")};
+  opacity: ${({ show }) => (show ? 1 : 0)};
+  visibility: ${({ show }) => (show ? "visible" : "hidden")};
+  -webkit-backface-visibility: ${({ show }) => (show ? "visible" : "hidden")};
+  -moz-backface-visibility: ${({ show }) => (show ? "visible" : "hidden")};
+  backface-visibility: ${({ show }) => (show ? "visible" : "hidden")};
+
+  .header {
+    font-family: "Nunito", sans-serif;
+    font-size: 23px;
+    font-weight: 700;
+    color: #3c404a;
+    margin: 50px 60px 0;
+  }
+
+  .form {
+    display: flex;
+    align-items: flex-start;
+    margin: 50px 60px;
+    img {
+      margin-right: 30px;
+    }
+  }
+
+  .inputs {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .image {
+    width: 260px;
+    height: 260px;
+    border-radius: 20px;
+  }
+
+  .buttons {
+    align-self: end;
+    margin-right: 105px;
+    margin-top: 30px;
+  }
+
+  .cancel {
+    border: none;
+    width: 140px;
+    color: #b4b1b1;
+    font-size: 16px;
+    cursor: pointer;
+    margin-right: 20px;
+    background: transparent;
+    font-family: "Nunito", sans-serif;
+  }
+  .apply {
+    border: none;
+    width: 140px;
+    color: #3c404a;
+    font-size: 16px;
+    cursor: pointer;
+    background: transparent;
+    font-family: "Nunito", sans-serif;
+  }
+`;
+
+export default RiverEditFormStyled;
