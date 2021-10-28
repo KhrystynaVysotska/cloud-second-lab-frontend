8a434d7aae6c61a34560691133ac6b1c20c4ccd1
Khrystyna <khrystynavysotska@gmail.com>
2021-10-28 04:23:03 +0300

    Added new reducers

--- src/common/business/redux/rootReducer.js
+++ src/common/business/redux/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import measurementPointReducer from "../../../measurement_points/business/redux/measurementPointSlice";
 import riverReducer from "../../../rivers/business/redux/riverSlice";
+import sensorReducer from "../../../float_sensors/business/redux/floatSensorSlice";
 
 const rootReducer = combineReducers({
   rivers: riverReducer,
+  sensors: sensorReducer,
+  measurementPoints: measurementPointReducer,
 });
 
 export default rootReducer;
