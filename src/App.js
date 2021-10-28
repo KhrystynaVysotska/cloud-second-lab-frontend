3ef0e69a056e7d4f43b006f77dd96dcf2a502be7
Khrystyna <khrystynavysotska@gmail.com>
2021-10-28 04:22:28 +0300

    Moved latest measurements panel to the main app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import "./App.css";
+import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
 import Layout from "./common/presentation/pages/Layout";
+import { getDbLatestMeasurements } from "./measurements/api/measurementDbApi";
+import LatestMeasurementCardCarousel from "./measurements/presentation/components/LatestMeasurementCardCarousel";
+import AppStyled from "./styles/App.styled";
 
 function App() {
-  return <Layout />;
+  const dispatch = useDispatch();
+  const [latestMeasurements, setLatestMeasurements] = useState([]);
+
+  useEffect(() => {
+    getDbLatestMeasurements()
+      .then(({ data }) => setLatestMeasurements(data))
+      .catch((err) => console.log(err));
+  }, [dispatch]);
+
+  return (
+    <AppStyled>
+      <LatestMeasurementCardCarousel measurements={latestMeasurements} />
+      <Layout latestMeasurements={latestMeasurements} />
+    </AppStyled>
+  );
 }
 
 export default App;
---
+++ src/styles/App.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  width: 920px;
+  margin: 25px auto;
+  background-color: #f9f9fd;
+`;
+
+export default AppStyled;
